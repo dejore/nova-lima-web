bf31c07ed30207c5fe1828f4b81e9889f5470062
dejore <dejore@dejore.com.br>
2018-02-05 23:22:33 -0200

    commit Inicial

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./../node_modules/normalize.css";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(<p>Hello</p>, document.getElementById("root"));
+registerServiceWorker();
